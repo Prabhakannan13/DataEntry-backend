cfdaee2636209f704cd4804b30f9964d81f5f2a1
Lakshmi <Lakshmi@NTPLAP-008.chn.nestgroup.net>
2022-10-20 14:23:44 +0530

    push

--- src/main/java/com/nestdigital/studentbackend/Dao/DataentryDao.java
+++ src/main/java/com/nestdigital/studentbackend/Dao/DataentryDao.java
@@ -5,9 +5,14 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface DataentryDao extends CrudRepository<DataentryModel,Integer> {
 
     @Modifying
     @Query(value = "DELETE FROM `dataentry` WHERE `id`=:id",nativeQuery = true)
     void deleteDataById(Integer id);
+
+    @Query(value = "SELECT `id`, `clubcolour`, `clubname`, `moto`, `studentname` FROM `dataentry` WHERE `clubname`=:clubname",nativeQuery = true)
+    List<DataentryModel>searchData(String clubname);
 }
